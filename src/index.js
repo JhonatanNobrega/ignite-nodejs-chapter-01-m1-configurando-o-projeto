f2db28eda5924df28a51d1db248dc68a0e401e0d
Jhonatan Nobrega <jhonatan_nobrega@hotmail.com>
2022-03-06 00:20:04 -0300

    Ultilizando os metodos HTTP

--- src/index.js
+++ src/index.js
@@ -6,4 +6,25 @@ app.get('/', (req, res) => {
   return res.send('Hello, world! Ignite')
 })
 
+/**
+ * Métodos HTTP
+ */
+
+app.get('/courses', (req, res)=>{
+  // Busca simples no servidor
+})
+
+app.post('/courses', (req, res)=>{
+  // Inserir dados no servidor
+})
+
+app.put('/courses/:id', (req, res)=>{
+  // Modificar dados no servidor
+})
+
+app.patch('/courses/:id', (req, res)=>{
+  // Modificar dado especifico no servidor
+})
+
+
 app.listen(3333)
