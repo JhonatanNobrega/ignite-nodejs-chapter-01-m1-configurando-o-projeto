9c6162837f9d0fd87d61f63496e469a220aaf4ca
Jhonatan Nobrega <jhonatan_nobrega@hotmail.com>
2022-03-06 23:22:04 -0300

    Chapter 01 - Modulo 1/Configurando o projeto - tipos de parametros

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   return res.send('Hello, world! Ignite')
 })
@@ -9,22 +11,30 @@ app.get('/', (req, res) => {
 /**
  * Métodos HTTP
  */
-
 app.get('/courses', (req, res)=>{
   // Busca simples no servidor
+  return res.send('Feito')
 })
 
 app.post('/courses', (req, res)=>{
   // Inserir dados no servidor
+  console.log(req.params);
+  return res.send('Feito')
 })
 
 app.put('/courses/:id', (req, res)=>{
   // Modificar dados no servidor
+  return res.send('Feito')
 })
 
 app.patch('/courses/:id', (req, res)=>{
   // Modificar dado especifico no servidor
+  return res.send('Feito')
 })
 
+app.delete('/courses/:id', (req, res)=>{
+  // Modificar dado especifico no servidor
+  return res.send('Feito')
+})
 
 app.listen(3333)
